9725744e21b20d61fc3ac0fdff886d0c3185b69f
pxomaker-hue <pxomaker@gmail.com>
2026-06-04 19:36:51 +0200

    ne version stellar test

--- static/js/main.js
+++ static/js/main.js
@@ -6,118 +6,134 @@
 
 (function($) {
 
-	var	$window = $(window),
-		$body = $('body'),
-		$main = $('#main');
+	skel.breakpoints({
+		xlarge: '(max-width: 1680px)',
+		large: '(max-width: 1280px)',
+		medium: '(max-width: 980px)',
+		small: '(max-width: 736px)',
+		xsmall: '(max-width: 480px)',
+		xxsmall: '(max-width: 360px)'
+	});
 
-	// Breakpoints.
-		breakpoints({
-			xlarge:   [ '1281px',  '1680px' ],
-			large:    [ '981px',   '1280px' ],
-			medium:   [ '737px',   '980px'  ],
-			small:    [ '481px',   '736px'  ],
-			xsmall:   [ '361px',   '480px'  ],
-			xxsmall:  [ null,      '360px'  ]
-		});
+	$(function() {
 
-	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
+		var	$window = $(window),
+			$body = $('body'),
+			$main = $('#main');
 
-	// Nav.
-		var $nav = $('#nav');
+		// Disable animations/transitions until the page has loaded.
+			$body.addClass('is-loading');
 
-		if ($nav.length > 0) {
+			$window.on('load', function() {
+				window.setTimeout(function() {
+					$body.removeClass('is-loading');
+				}, 100);
+			});
 
-			// Shrink effect.
-				$main
-					.scrollex({
-						mode: 'top',
-						enter: function() {
-							$nav.addClass('alt');
-						},
-						leave: function() {
-							$nav.removeClass('alt');
-						},
-					});
+		// Fix: Placeholder polyfill.
+			$('form').placeholder();
 
-			// Links.
-				var $nav_a = $nav.find('a');
+		// Prioritize "important" elements on medium.
+			skel.on('+medium -medium', function() {
+				$.prioritize(
+					'.important\\28 medium\\29',
+					skel.breakpoint('medium').active
+				);
+			});
 
-				$nav_a
-					.scrolly({
-						speed: 1000,
-						offset: function() { return $nav.height(); }
-					})
-					.on('click', function() {
+		// Nav.
+			var $nav = $('#nav');
 
-						var $this = $(this);
+			if ($nav.length > 0) {
 
-						// External link? Bail.
-							if ($this.attr('href').charAt(0) != '#')
-								return;
+				// Shrink effect.
+					$main
+						.scrollex({
+							mode: 'top',
+							enter: function() {
+								$nav.addClass('alt');
+							},
+							leave: function() {
+								$nav.removeClass('alt');
+							},
+						});
 
-						// Deactivate all links.
-							$nav_a
-								.removeClass('active')
-								.removeClass('active-locked');
+				// Links.
+					var $nav_a = $nav.find('a');
 
-						// Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
-							$this
-								.addClass('active')
-								.addClass('active-locked');
+					$nav_a
+						.scrolly({
+							speed: 1000,
+							offset: function() { return $nav.height(); }
+						})
+						.on('click', function() {
 
-					})
-					.each(function() {
+							var $this = $(this);
 
-						var	$this = $(this),
-							id = $this.attr('href'),
-							$section = $(id);
+							// External link? Bail.
+								if ($this.attr('href').charAt(0) != '#')
+									return;
 
-						// No section for this link? Bail.
-							if ($section.length < 1)
-								return;
+							// Deactivate all links.
+								$nav_a
+									.removeClass('active')
+									.removeClass('active-locked');
 
-						// Scrollex.
-							$section.scrollex({
-								mode: 'middle',
-								initialize: function() {
+							// Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
+								$this
+									.addClass('active')
+									.addClass('active-locked');
 
-									// Deactivate section.
-										if (browser.canUse('transition'))
-											$section.addClass('inactive');
+						})
+						.each(function() {
 
-								},
-								enter: function() {
+							var	$this = $(this),
+								id = $this.attr('href'),
+								$section = $(id);
 
-									// Activate section.
-										$section.removeClass('inactive');
+							// No section for this link? Bail.
+								if ($section.length < 1)
+									return;
 
-									// No locked links? Deactivate all links and activate this section's one.
-										if ($nav_a.filter('.active-locked').length == 0) {
+							// Scrollex.
+								$section.scrollex({
+									mode: 'middle',
+									initialize: function() {
 
-											$nav_a.removeClass('active');
-											$this.addClass('active');
+										// Deactivate section.
+											if (skel.canUse('transition'))
+												$section.addClass('inactive');
 
-										}
+									},
+									enter: function() {
 
-									// Otherwise, if this section's link is the one that's locked, unlock it.
-										else if ($this.hasClass('active-locked'))
-											$this.removeClass('active-locked');
+										// Activate section.
+											$section.removeClass('inactive');
 
-								}
-							});
+										// No locked links? Deactivate all links and activate this section's one.
+											if ($nav_a.filter('.active-locked').length == 0) {
 
-					});
+												$nav_a.removeClass('active');
+												$this.addClass('active');
 
-		}
+											}
 
-	// Scrolly.
-		$('.scrolly').scrolly({
-			speed: 1000
-		});
+										// Otherwise, if this section's link is the one that's locked, unlock it.
+											else if ($this.hasClass('active-locked'))
+												$this.removeClass('active-locked');
+
+									}
+								});
+
+						});
+
+			}
+
+		// Scrolly.
+			$('.scrolly').scrolly({
+				speed: 1000
+			});
+
+	});
 
 })(jQuery);
